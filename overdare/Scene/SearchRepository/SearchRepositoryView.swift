23be90b13eaee11b298423bcc44b2f18811c5be4
krindale <wow32@hotmail.com>
2024-02-19 06:02:42 +0900

    간단 search bar 생성
    searchable 사용 하여 간단 한 search 기능 사용

--- overdare/Scene/SearchRepository/SearchRepositoryView.swift
+++ overdare/Scene/SearchRepository/SearchRepositoryView.swift
@@ -13,32 +13,37 @@ struct SearchRepositoryView: View {
     
     var body: some View {
         NavigationStack() {
-            List(viewModel.searchRepositoryResult?.items ?? []) { repo in
-                NavigationLink(value: repo) {
-                    VStack(alignment: .leading) {
-                        Text(repo.fullName).font(.headline)
-                        HStack {
-                            Text(repo.description ?? "No description")
-                                .font(.subheadline)
-                                .lineLimit(1)
-                            Text("By")
-                            Text(repo.owner.login)
-                                .font(.subheadline)
-                                .lineLimit(1)
+            VStack {
+                List(viewModel.searchRepositoryResult?.items ?? []) { repo in
+                    NavigationLink(value: repo) {
+                        VStack(alignment: .leading) {
+                            Text(repo.fullName).font(.headline)
+                            HStack {
+                                Text(repo.description ?? "No description")
+                                    .font(.subheadline)
+                                    .lineLimit(1)
+                                Text("By")
+                                Text(repo.owner.login)
+                                    .font(.subheadline)
+                                    .lineLimit(1)
+                            }
+                            HStack {
+                                TagView(type: .fork(count: repo.forksCount))
+                                TagView(type: .star(count: repo.stargazersCount))
+                            }.font(.caption)
                         }
-                        HStack {
-                            TagView(type: .fork(count: repo.forksCount))
-                            TagView(type: .star(count: repo.stargazersCount))
-                        }.font(.caption)
                     }
+                }.listStyle(.plain)
+                .navigationTitle("Repository")
+                .searchable( // <-
+                  text: $viewModel.searchText,
+                  placement: .automatic,
+                  prompt: "Repository Search"
+                )
+                .navigationDestination(for: Repository.self) { repo in
+                    RepositoryDetailView(viewModel: .init(owner: repo.owner.login, repo: repo.name))
                 }
-            }.navigationTitle("Repository")
-            .navigationDestination(for: Repository.self) { repo in
-                RepositoryDetailView(viewModel: .init(owner: repo.owner.login, repo: repo.name))
             }
-        }
-        .task {
-            await viewModel.fetchRepositry(searchText: "helloworld")
         }
      }
 }
--- overdare/Scene/SearchRepository/SearchRepositoryViewModel.swift
+++ overdare/Scene/SearchRepository/SearchRepositoryViewModel.swift
@@ -6,11 +6,32 @@
 //
 
 import Foundation
+import Combine
 
 final class SearchRepositoryViewModel: ObservableObject {
     @Published var searchRepositoryResult: GitHubSearchResponse?
+    @Published var searchText: String = ""
     
-    func fetchRepositry(searchText: String) async {
+    private var cancellables: Set<AnyCancellable> = .init()
+    
+    init() {
+        self.$searchText
+            .debounce(for: .seconds(0.3), scheduler: RunLoop.main)
+            .receive(on: DispatchQueue.global())
+            .sink { searchText in
+                Task { [ weak self] in
+                    if searchText.isEmpty {
+                        await MainActor.run { [weak self] in
+                            self?.searchRepositoryResult = nil
+                        }
+                    } else {
+                        await self?.fetchRepositry(with: searchText)
+                    }
+                }
+            }.store(in: &cancellables)
+    }
+    
+    func fetchRepositry(with searchText: String) async {
         do {
             let searchRepositoryResult = try await APIManager.shared.fetchGithubSearch(with: searchText)
             
