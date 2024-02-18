c3a08287d4c3c7c04bdea294ee7fb257b663d9b5
krindale <wow32@hotmail.com>
2024-02-19 00:57:15 +0900

    project init

---
+++ overdare/overdareApp.swift
@@ -0,0 +1,17 @@
+//
+//  overdareApp.swift
+//  overdare
+//
+//  Created by Eddie on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct overdareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
